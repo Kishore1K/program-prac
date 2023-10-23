2459aeadfbc7eaaca8b5e03133bc0de7ac6e0bc5
Kishore1K <reddykishore527@gmail.com>
2023-10-23 20:01:23 +0530

    Pattern programs

---
+++ interview/Pattern.java
@@ -0,0 +1,129 @@
+package DSA.interview;
+
+public class Pattern {
+    public static void pattern1(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+        System.out.println("");
+
+        }
+    }
+    public static void pattern2(int n){
+        for(int i=n; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+
+            }
+        System.out.println("");
+        }
+    }
+    // Square hollow pattern 
+
+        public static void pattern3(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n;j++){
+                if(i==0 || j==0|| i==n-1|| j==n-1){
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+            
+        }
+    }
+        // Number triangle Pattern
+
+    public static void pattern4(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+    }
+    // Number-increasing Pyramid Pattern 
+    public static void pattern5(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    //  Number-increasing reverse Pyramid Pattern
+    public static void pattern6(int n){
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Number-changing Pyramid Pattern
+    public static void pattern7(int n){
+        int count=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                count++;
+                System.out.print(count+ " ");
+            }
+            System.out.println();
+        }
+    }
+// Zero-One Triangle Pattern
+    public static void pattern8(int n){
+        for(int i =1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print(1+ " ");
+                }else{
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    // Palindrome Triangle Pattern
+    public static void pattern9(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=2* (n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j>=1; j--){
+                System.out.print(j+" ");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    // Rhombus Pattern
+    public static void pattern10(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i;j++ ){
+                System.out.print(" ");
+            }
+            for(int j=0; j<n; j++ ){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        // pattern1(5);
+        // pattern2(5);
+        pattern10(6);
+
+    }
+}
