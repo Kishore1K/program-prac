a5b9a5bc0e024cdd55a12770e96a8cf658d0c203
Kishore1K <reddykishore527@gmail.com>
2023-10-23 20:27:55 +0530

    Pattern programs

--- interview/Pattern.java
+++ interview/Pattern.java
@@ -120,10 +120,82 @@ public class Pattern {
             System.out.println();
         }
     }
+
+    // Diamond Star Pattern
+    public static void pattern11(int n){
+         int i, j;
+         for (i = 1; i <= n; i++) { 
+            for (j = 1; j <= n - i; j++) { 
+                System.out.print(" "); 
+            } 
+            for (j = 1; j <= 2 * i - 1; j++) { 
+                System.out.print("*"); 
+            } 
+            System.out.println(); 
+        } 
+  
+        for (i = n-1; i >= 1; i--) { 
+            for (j = 1; j <= n - i; j++) { 
+                System.out.print(" "); 
+            } 
+            for (j = 1; j <= 2 * i - 1; j++) { 
+                System.out.print("*"); 
+            } 
+            System.out.println(); 
+            } 
+    }
+    static void pattern12(int n){
+        for(int i=n; i>=1; i--){
+            for(int j =1; j<=i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    // Left Half Pyramid Pattern
+    static void pattern13(int n){
+        int i, j; 
+  
+        // outer loop to handle rows 
+        for (i = n; i >= 1; i--) { 
+  
+            // inner loop to print spaces. 
+            for (j = 1; j < i; j++) { 
+                System.out.print(" "); 
+            } 
+  
+            // inner loop to print stars. 
+            for (j = 0; j <= n - i; j++) { 
+                System.out.print("*"); 
+            } 
+  
+            // printing new line for each row 
+            System.out.println(); 
+        } 
+    }
+    //  Pascal’s Triangle
+
+    public static void printPascal(int n) 
+    { 
+        for (int i = 1; i <= n; i++) { 
+            for (int j = 0; j <= n - i; j++) { 
+  
+                System.out.print(" "); 
+            } 
+  
+            int x = 1; 
+            for (int k = 1; k <= i; k++) { 
+                  System.out.print(x + " "); 
+                x = x * (i - k) / k; 
+            } 
+            System.out.println(); 
+        } 
+    } 
+
     public static void main(String[] args) {
         // pattern1(5);
         // pattern2(5);
-        pattern10(6);
+        printPascal(6);
 
     }
 }
