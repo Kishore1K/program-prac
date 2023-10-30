6ac222ea9df020a001168153cb2ff25c83bdfcb7
Kishore1K <reddykishore527@gmail.com>
2023-10-30 13:00:02 +0530

    delete operations

--- DataStructures/java/LinkedList/DoublyLinkedList.java
+++ DataStructures/java/LinkedList/DoublyLinkedList.java
@@ -73,6 +73,43 @@ public class DoublyLinkedList {
             System.out.print(curNode.data+"\t");
             curNode=curNode.prev;
         }
+        System.out.println();
+    }
+
+    void deleteAtBegining(){
+        Node cuNode=head;
+
+        head=head.next;
+        head.prev=null;
+        cuNode.next=null;
+    }
+    void deleteAtEnd(){
+        Node curNode=tail;
+        tail=tail.prev;
+        tail.next=null;
+        curNode.prev=null;
+    }
+    void deleteAtPos(int pos){
+        Node curNode=head;
+
+        if(pos==1){
+            deleteAtBegining();
+        }else{
+            int count=1;
+            while (curNode!=null && count<pos) {
+                curNode=curNode.next;
+                count++;
+            }
+            if(curNode==null){
+                deleteAtEnd();
+            }
+            else{
+                curNode.prev.next=curNode.next;
+                curNode.next.prev=curNode.prev;
+                curNode.next=curNode.prev=null;
+            }
+
+        }
     }
     public static void main(String[] args) {
     DoublyLinkedList linkedList = new DoublyLinkedList();
@@ -85,7 +122,14 @@ public class DoublyLinkedList {
     linkedList.printForward();
     linkedList.insertAtPos(250, 3);
     linkedList.printForward();
-    linkedList.printBackward();
+    // linkedList.printBackward();
+    linkedList.deleteAtBegining();
+    linkedList.printForward();
+    linkedList.deleteAtEnd();
+    linkedList.printForward();
+    linkedList.deleteAtPos(2);
+    linkedList.printForward();
+
 
 
     }
