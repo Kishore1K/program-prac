76b5c43fad0eefc5da07f9eb4924f8daf351093b
Kishore1K <reddykishore527@gmail.com>
2023-10-31 19:45:04 +0530

    Stack Operation

--- DataStructures/java/StackQueues/Stacks.java
+++ DataStructures/java/StackQueues/Stacks.java
@@ -3,9 +3,9 @@ package DSA.DataStructures.java.StackQueues;
 
 
 public class Stacks {
-    private static int top;
-    private static int arr[];
-    private static int cap;
+    private  int top;
+    private  int arr[];
+    private  int cap;
 
     Stacks(int size){
         arr = new int[size];
@@ -14,14 +14,31 @@ public class Stacks {
 
     }
 
-    static void push(int data){
+    void push(int data){
         if(top==cap){
             System.out.println("Stack OverFlow");
+            return;
         }
-        arr[top++]=data;
+        top++;
+        arr[top]=data;
 
     }
+    void print(int arr[]){
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.print(arr[i]+"\t");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
+        Stacks stacks = new Stacks(5);
+        stacks.push(20);
+        stacks.push(30);
+        stacks.push(40);
+        stacks.push(50);
+        stacks.push(60);
+
+        stacks.print(stacks.arr);
+
         
     }
 }
