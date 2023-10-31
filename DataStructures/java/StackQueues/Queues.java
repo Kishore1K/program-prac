789ef527e3b4846583e34183ad971c8aecb6d280
Kishore1K <reddykishore527@gmail.com>
2023-10-31 19:59:28 +0530

    Stack Operation

---
+++ DataStructures/java/StackQueues/Queues.java
@@ -0,0 +1,12 @@
+package DSA.DataStructures.java.StackQueues;
+
+public class Queues {
+    private int front;
+    private int rear;
+    private int arr[];
+    private int cap;
+
+    Queues(int size){
+        
+    }
+}
