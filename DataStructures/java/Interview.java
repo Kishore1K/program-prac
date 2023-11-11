8d7a63ea57cd9517c6eb2a3291503e41a8702e22
Kishore1K <reddykishore527@gmail.com>
2023-11-11 12:26:23 +0530

    Java Prgms

--- DataStructures/java/Interview.java
+++ DataStructures/java/Interview.java
@@ -48,10 +48,51 @@ public class Interview{
         System.out.println(sum);
     }
 
+    static void printNum(){
+        for(int i=0; i<10;i++){
+            System.out.print(i+" ");
+
+        }
+        System.out.println();
+    }    
+    static void printNumRev(){
+        for(int i=10; i>=0;i--){
+            System.out.print(i+" ");
+
+        }
+        System.out.println();
+    }
+
+    static void generateOTP(){
+        int randNum = (int)(Math.random()*9000)+1000;
+        String otp= String.valueOf(randNum);
+        System.out.println(otp);
+    }
+    static void details(){
+        System.out.println("Java Version     : "+System.getProperty("java.version"));
+        System.out.println("Java VM Version  : "+System.getProperty("java.vm.version"));
+        System.out.println("Java VM Vendor   : "+System.getProperty("java.vm.vendor"));
+        System.out.println("Java VM Name     : "+System.getProperty("java.vm.name"));
+        System.out.println("Java VM specification Version : "+System.getProperty("java.vm.specification.version"));
+        System.out.println("Java VM specification Vendor  : "+System.getProperty("java.vm.specification.vendor"));
+        System.out.println("Java VM specification Name    : "+System.getProperty("java.vm.specification.name"));
+        System.out.println("Java Runtime specification Version : "+System.getProperty("java.specification.version"));
+        System.out.println("Java Runtime specification Vendor  : "+System.getProperty("java.specification.vendor"));
+        System.out.println("Java Runtime specification Name    : "+System.getProperty("java.specification.name"));
+    }
+
     public static void main(String[] args) {
         primeOrNO(6);
         dec2oct(222);
         dec2bin(10);
         sumOfDigits(121);
+
+        // Thread t1 = new Thread(Interview::generateOTP);
+        // Thread t2 = new Thread(Interview::printNumRev);
+        // t1.start();
+        // t2.start();
+        generateOTP();
+        details();
+
     }
 }
