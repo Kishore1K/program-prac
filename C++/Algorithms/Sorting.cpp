eabe28df8132defdd1bf12b156ca20f9db318b2a
Kishore1K <reddykishore527@gmail.com>
2023-12-09 11:01:05 +0530

    Sorting

--- C++/Algorithms/Sorting.cpp
+++ C++/Algorithms/Sorting.cpp
@@ -107,6 +107,37 @@ public:
         mergeSort(arr, mid + 1, high);
         merge(arr, low, mid, high);
     } /*Merge Sort*/
+    int partition(vector<int> &arr, int low, int high)
+    {
+        int povit = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j)
+        {
+            while (arr[i] <= povit && i <= high - 1)
+            {
+                i++;
+            }
+            while (arr[j] > povit && j >= low + 1)
+            {
+                j--;
+            }
+            if (i < j)
+                swap(arr[i], arr[j]);
+        }
+        swap(arr[low], arr[high]);
+        return j;
+    }
+    void quickSort(vector<int> &arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    } /*quick Sort*/
     void printArray(int arr[], int n)
     {
         for (int i = 0; i < n; i++)
@@ -120,10 +151,16 @@ int main()
 {
     Sorting sol;
     int arr[] = {4, 8, 7, 3, 6, 9, 7, 1};
+    vector<int> arr1 = {4, 8, 7, 3, 6, 9, 7, 1};
+
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Before Sorting" << endl;
     sol.printArray(arr, n);
     cout << "After Sorting" << endl;
-    sol.mergeSort(arr, 0, n);
-    sol.printArray(arr, n);
+    sol.quickSort(arr1, 0, arr1.size() - 1);
+    for (auto it = arr1.begin(); it != arr1.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
 }
