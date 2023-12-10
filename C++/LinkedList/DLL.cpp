a1c21842b533aeb1ef256fb42e9f94ac3d007f03
Kishore1K <reddykishore527@gmail.com>
2023-12-10 18:06:17 +0530

    DLL

--- C++/LinkedList/DLL.cpp
+++ C++/LinkedList/DLL.cpp
@@ -26,6 +26,7 @@ DLL *insertAtFirst(int data, DLL *head)
     if (head == NULL)
     {
         head = newNode;
+        newNode->next = newNode->prev = NULL;
         return head;
     }
     else
@@ -42,16 +43,57 @@ DLL *insertAtEnd(int data, DLL *head)
     DLL *newNode = new DLL(data);
     DLL *temp = head;
 
-    while (temp->next->next != NULL)
+    while (temp->next != NULL)
     {
         temp = temp->next;
     }
     newNode->next = NULL;
-    temp->next->next = newNode;
+    temp->next = newNode;
     newNode->prev = temp;
     return head;
 }
-
+DLL *insertAtPos(int data, int pos, DLL *head)
+{
+    DLL *temp = head;
+    int count = 0;
+    DLL *newNode = new DLL(data);
+    while (temp != NULL)
+    {
+        count++;
+        if (count == pos)
+        {
+            break;
+        }
+        temp = temp->next;
+    }
+    newNode->prev = temp;
+    newNode->next = temp->next;
+    temp->next = newNode;
+    newNode->next->prev = newNode;
+    return head;
+}
+DLL *deleteAtFirst(DLL *head)
+{
+    DLL *temp = head;
+    head = temp->next;
+    temp->next->prev = NULL;
+    delete temp;
+    return head;
+}
+DLL *deleteAtEnd(DLL *head)
+{
+    DLL *temp = head;
+    DLL *prev;
+    while (temp->next != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next->prev = NULL;
+    prev->next = NULL;
+    delete temp;
+    return head;
+}
 void printDLL(DLL *head)
 {
     DLL *temp = head;
@@ -62,6 +104,23 @@ void printDLL(DLL *head)
     }
     cout << endl;
 }
+
+void printDLLReverse(DLL *head)
+{
+    DLL *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    cout << temp->val << " ";
+    while (temp->prev != NULL)
+    {
+        temp = temp->prev;
+        cout << temp->val << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     DLL *head = NULL;
@@ -71,5 +130,10 @@ int main()
     head = insertAtFirst(40, head);
     head = insertAtFirst(50, head);
     head = insertAtEnd(60, head);
+    head = insertAtPos(100, 3, head);
     printDLL(head);
+    printDLLReverse(head);
+    head = deleteAtEnd(head);
+    printDLL(head);
+    printDLLReverse(head);
 }
