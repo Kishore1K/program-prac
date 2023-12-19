eab119f2e8e54bb427e720215313fde902bed7dc
Kishore1K <reddykishore527@gmail.com>
2023-12-19 21:14:54 +0530

    OOPS

---
+++ C++/OOPS/pointer2Obj.cpp
@@ -0,0 +1,30 @@
+// Accessing member using pointer object
+
+#include <bits/stdc++.h>
+using namespace std;
+class Laptop
+{
+    int Ram;
+
+public:
+    int usb;
+    void getdata(int a)
+    {
+        Ram = a;
+    }
+    void disp()
+    {
+        cout << "RAM: " << Ram << endl;
+        cout << "USB: " << usb << endl;
+    }
+};
+int main()
+{
+    Laptop dell;
+    Laptop *pdell;
+    pdell = &dell;
+    pdell->usb = 3; // (*pdell).usb=3
+    pdell->getdata(20);
+    pdell->disp();
+    return 0;
+}
